fda7f8378604910b25a83f6430f24f0e08f825bf
= <yuda.pinhas@skai.io>
2025-05-23 11:31:17 +0300

    trigger-ci

--- backend.tf
+++ backend.tf
@@ -1,7 +1,11 @@
 terraform {
   required_version = ">= 1.0.0"
 
-  backend "local" {
-    path = "terraform.tfstate"
+  backend "remote" {
+    organization = "yudapinhas"
+
+    workspaces {
+      name = "dev"
+    }
   }
 }
